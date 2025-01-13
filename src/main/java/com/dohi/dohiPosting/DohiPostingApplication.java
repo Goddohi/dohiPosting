d3dfb35fd35650ad4d70c9b5ef22b7cc8a78d4c3
goddohi <kmam147@gmail.com>
2025-01-14 01:38:30 +0900

    기본설정

---
+++ src/main/java/com/dohi/dohiPosting/DohiPostingApplication.java
@@ -0,0 +1,13 @@
+package com.dohi.dohiPosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DohiPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DohiPostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dohi/dohiPosting/DohiPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dohi.dohiPosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DohiPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
